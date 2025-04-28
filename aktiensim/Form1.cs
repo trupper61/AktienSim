243845dfde97dac7fa0a9424c35926b96c2afd25
feuerdoge <deniboz11@gmail.com>
2025-04-28 14:09:58 +0200

    People are Addable
    -Man kann Personen hinzufügen

--- aktiensim/Form1.cs
+++ aktiensim/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -9,6 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data;
+using MySql.Data.MySqlClient;
 namespace aktiensim
 {
     public partial class Form1 : Form
@@ -55,12 +57,32 @@ namespace aktiensim
             }
             string passHash = Hash(password);
             MessageBox.Show(passHash);
+
+            BenutzerAnlegen(email, vName, nName, password, passwdCheck);
         }
         //Credits: https://stackoverflow.com/questions/17292366/hashing-with-sha1-algorithm-in-c-sharp
         static string Hash(string input)
         {
             var hash = new SHA1Managed().ComputeHash(Encoding.UTF8.GetBytes(input));
             return string.Concat(hash.Select(b => b.ToString("x2")));
+        }
+
+        public void BenutzerAnlegen(string email, string vName, string nName, string password, string passwdCheck) 
+        {
+            string connString = "server=localhost;database=aktiensimdb;uid=root;password=\"\"";
+            MySqlConnection conn = new MySqlConnection(connString);
+            conn.Open();
+
+            string qry = "INSERT INTO benutzer(Name, Vorname, Email, MitgliedSeit) VALUES(@nName, @vName, @email, @date)";
+
+            using (MySqlCommand cmd = new MySqlCommand(qry, conn)) 
+            {
+                cmd.Parameters.AddWithValue("nName", nName);
+                cmd.Parameters.AddWithValue("vName", vName);
+                cmd.Parameters.AddWithValue("email", email);
+                cmd.Parameters.AddWithValue("date", DateTime.Now);
+                cmd.ExecuteNonQuery();
+            }
         }
     }
 }
