f60f63885cc375f62351a9cf95ce5d7e86eb6acf
trupper61 <152274189+trupper61@users.noreply.github.com>
2025-04-28 12:52:32 +0200

    Added register interface

--- aktiensim/Form1.cs
+++ aktiensim/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,54 @@ namespace aktiensim
 {
     public partial class Form1 : Form
     {
+        public Panel loginPanel;
         public Form1()
         {
             InitializeComponent();
+        }
+        public void InitUi()
+        {
+            loginPanel = new Panel
+            {
+                Size = new Size(this.ClientSize.Width, this.ClientSize.Height),
+                Location = new Point(0, 0),
+                BackColor = Color.LightBlue
+            };
+            TextBox box = new TextBox
+            {
+                Size = new Size(100, 30),
+                Location = new Point((loginPanel.Width - 80)/2, (loginPanel.Height -80)/2),
+                Text = "Email..."
+            };
+            loginPanel.Controls.Add(box);
+            Controls.Add(loginPanel);
+        }
+
+        private void RegisterBtn_Click(object sender, EventArgs e)
+        {
+            string email = emailInput.Text;
+            string vName = vornameInput.Text;
+            string nName = nachnameInput.Text;
+            string password = passwortInput.Text;
+            string passwdCheck = passwortCheck.Text;
+            if (email == "" || vName == "" || nName == "" || password == "" || passwdCheck == "")
+            {
+                MessageBox.Show("Alle Felder wurden nicht ausgefüllt");
+                return;
+            }
+            if (password != passwdCheck)
+            {
+                MessageBox.Show("Passwörter stimmen nicht über ein!");
+                return;
+            }
+            string passHash = Hash(password);
+            MessageBox.Show(passHash);
+        }
+        //Credits: https://stackoverflow.com/questions/17292366/hashing-with-sha1-algorithm-in-c-sharp
+        static string Hash(string input)
+        {
+            var hash = new SHA1Managed().ComputeHash(Encoding.UTF8.GetBytes(input));
+            return string.Concat(hash.Select(b => b.ToString("x2")));
         }
     }
 }
