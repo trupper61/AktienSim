70622912980ca638f2f38e72398761aa2cb6044d
feuerdoge <deniboz11@gmail.com>
2025-05-12 12:29:39 +0200

    BenutzerID fix
    Probleme mit der BenutzerID behoben

---
+++ aktiensim/Benutzer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aktiensim
+{
+    public class Benutzer
+    {
+        public string name;
+        public string vorname;
+        public string email;
+        public int benutzerID;
+    }
+}
